f01e4f4bac4151a967b0786094f431e51aa7833c
Jose Javier Mirador <josejaviersmirador@gmail.com>
2026-02-17 00:13:28 +0000

    updated app.jsx

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,14 +1,7 @@
-import React from 'react';
-
 function App() {
-  const myTitle = "My New React JSX App";
-
   return (
-    <div style={{ padding: '20px', textAlign: 'center', fontFamily: 'Arial' }}>
-      <h1>{myTitle}</h1>
-      <p>This is my updated Vite-React website with sample code.</p>
-      <hr />
-      <p>Ready for Task 04 API calls!</p>
+    <div className="App">
+      <h1>Hello!! Jv here!</h1>
     </div>
   );
 }
